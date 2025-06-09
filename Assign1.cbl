0b5db3718ba34bf070ba4c1524cf60bb8597b49d
Arthur Scharf <artemisscharf@gmail.com>
2025-06-09 19:02:58 -0400

    Finalized YEARS-OF-SERVICE input strategy. Refactored for readability

--- Assign1.cbl
+++ Assign1.cbl
@@ -3,7 +3,9 @@
       * Student ID: 040797015
       * Course & Section  CST8283_310
       * Date:  June 9th, 2025
-      * Purpose: Writes employee data to a file. Written as an exercise
+      * Purpose: Writes employee data to a file.
+      *          Reads and displays employees who've been with the
+      *          company for more than 10.5 years.
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. ASSIGNMENT-1.
@@ -15,6 +17,8 @@
               ORGANIZATION IS LINE SEQUENTIAL.
 
 
+      * Procedure names should be what they're going to do to the state
+
        DATA DIVISION.
        FILE SECTION.
       * File descriptor for Employee File. We'll read and write to this
@@ -22,6 +26,7 @@
        FD  EMPLOYEE-FILE.
        01  EMPLOYEE-FILE-LINE PIC X(55).
 
+      *
        WORKING-STORAGE SECTION.
        01  EMPLOYEE-RECORD.
            05  EMPLOYEE-ID         PIC 9(6).
@@ -29,6 +34,7 @@
            05  LAST-NAME           PIC A(20).
            05  FIRST-NAME          PIC A(20).
            05  YEARS-OF-SERVICE    PIC 99V9.
+
       * Used for formatted display
        01  EMPLOYEE-RECORD-OUT.
            05 EMPLOYEE-ID-OUT      PIC 9(6).
@@ -39,7 +45,8 @@
            05 FILLER               PIC X(3)   VALUE SPACES.
            05 FIRST-NAME-OUT       PIC A(20).
            05 FILLER               PIC X(3)   VALUE SPACES.
-           05 YEARS-OF-SERVICE-OUT PIC 99.9.
+           05 YEARS-OF-SERVICE-OUT PIC 99v9.
+
       * Simple header structure used for displaying.
       * I'm getting a warning that the string won't fit in the picture
       * string. The '|' character seems to cause it, yet the program
@@ -50,12 +57,18 @@
            05 H3 PIC A(23)  VALUE "Last                 | ".
            05 H4 PIC A(23)  VALUE "First                | ".
            05 H5 PIC A(16)  VALUE "Years of Service".
+
       * Used for display clarity
        01  DASH-LINE PIC X(84) VALUE ALL "=".
       * flag for exiting loop
        01  FLG-LOOPING         PIC A VALUE 'Y'.
       * Used for user control input
        01  CHOICE                PIC A.
+
+      * An explixit decimal was needed for parsing keyboard input for
+      * years of service
+       01  TEMP-YOS PIC 99.9.
+
 
 
        PROCEDURE DIVISION.
@@ -64,44 +77,34 @@
        100-MAIN-PROCEDURE.
            DISPLAY "---- TAKING INPUT ----".
            PERFORM 200-INIT-WRITE.
-           PERFORM 201-TAKE-INPUT UNTIL FLG-LOOPING NOT = 'Y'.
+           PERFORM 201-CREATE-EMPLOYEE-RECORD
+              UNTIL FLG-LOOPING NOT = 'Y'.
            PERFORM 204-IO-CLEANUP.
            DISPLAY " "
            DISPLAY "---- DISPLAYING RESULTS ----"
            PERFORM 202-INIT-READ.
            DISPLAY DASH-LINE.
-           PERFORM 203-SHOW-OUTPUT UNTIL FLG-LOOPING = 'N'.
+           PERFORM 203-READ-EMPLOYEE-RECORD UNTIL FLG-LOOPING = 'N'.
            DISPLAY DASH-LINE.
            PERFORM 204-IO-CLEANUP.
            222-TEST-READ.
            STOP RUN.
 
+
       * Inits writing. initializes EMPLOYEE-FILE and sets flag to
       * allow looping input
        200-INIT-WRITE.
            OPEN OUTPUT EMPLOYEE-FILE.
-           DISPLAY "-- New Record? (Y/N) --"
-           ACCEPT FLG-LOOPING.
+           PERFORM 303-POLL-FOR-CONTINUE.
+
 
       * The body of indefinite loop that takes input form the keyboard
-       201-TAKE-INPUT.
-           DISPLAY "Enter Employee ID (999999)".
-           ACCEPT EMPLOYEE-ID.
-           DISPLAY "Enter Deparment Code (999)".
-           ACCEPT DEPARTMENT-CODE.
-           DISPLAY "Enter Last Name".
-           ACCEPT LAST-NAME.
-           DISPLAY "Enter First Name".
-           ACCEPT FIRST-NAME.
-           DISPLAY "Enter Years of Service (99.9)".
-           ACCEPT YEARS-OF-SERVICE.
-           DISPLAY "-- Write? (Y/N) --".
-           ACCEPT CHOICE.
-           IF CHOICE = 'Y' THEN
-              MOVE EMPLOYEE-RECORD TO EMPLOYEE-FILE-LINE
-              WRITE EMPLOYEE-FILE-LINE
-           DISPLAY "-- New Record? (Y/N) --".
-           ACCEPT FLG-LOOPING.
+       201-CREATE-EMPLOYEE-RECORD.
+           PERFORM 302-ACCEPT-INPUT.
+           IF CHOICE = 'Y' OR CHOICE = 'y' THEN
+              WRITE EMPLOYEE-FILE-LINE FROM EMPLOYEE-RECORD.
+           PERFORM 303-POLL-FOR-CONTINUE.
+
 
       * Inits reading. Initializes EMPLOYEE-FILE and sets flag if
       * initial data exists
@@ -114,22 +117,48 @@
 
       * The body of a indefinite loop that reads lines from the
       * file and displays the output in a formatted way
-       203-SHOW-OUTPUT.
+       203-READ-EMPLOYEE-RECORD.
            MOVE EMPLOYEE-FILE-LINE TO EMPLOYEE-RECORD.
            IF YEARS-OF-SERVICE >= 10.5 THEN
-               MOVE EMPLOYEE-ID TO EMPLOYEE-ID-OUT
-               MOVE DEPARTMENT-CODE TO DEPARTMENT-CODE-OUT
-               MOVE LAST-NAME TO LAST-NAME-OUT
-               MOVE FIRST-NAME TO FIRST-NAME-OUT
-               MOVE YEARS-OF-SERVICE TO YEARS-OF-SERVICE-OUT
-               DISPLAY EMPLOYEE-RECORD-OUT.
+                  PERFORM 301-DISPLAY-RECORD.
            READ EMPLOYEE-FILE
               AT END MOVE 'N' TO FLG-LOOPING
            END-READ.
 
 
+      * Moves data to working-storage and displays it
+       301-DISPLAY-RECORD.
+           MOVE EMPLOYEE-ID TO EMPLOYEE-ID-OUT
+           MOVE DEPARTMENT-CODE TO DEPARTMENT-CODE-OUT
+           MOVE LAST-NAME TO LAST-NAME-OUT
+           MOVE FIRST-NAME TO FIRST-NAME-OUT
+           MOVE YEARS-OF-SERVICE TO YEARS-OF-SERVICE-OUT
+           DISPLAY EMPLOYEE-RECORD-OUT.
+
+      * Handles all the input decisions for creating a new record,
+      * including the checking if the user wants to create with the
+      * data that was entered
+       302-ACCEPT-INPUT.
+           DISPLAY "Enter Employee ID (999999)".
+           ACCEPT EMPLOYEE-ID.
+           DISPLAY "Enter Deparment Code (999)".
+           ACCEPT DEPARTMENT-CODE.
+           DISPLAY "Enter Last Name".
+           ACCEPT LAST-NAME.
+           DISPLAY "Enter First Name".
+           ACCEPT FIRST-NAME.
+           DISPLAY "Enter Years of Service (99.9)".
+           ACCEPT TEMP-YOS.
+           MOVE TEMP-YOS TO YEARS-OF-SERVICE.
+           DISPLAY "-- Write? (Y/N) --".
+           ACCEPT CHOICE.
+
+       303-POLL-FOR-CONTINUE.
+           DISPLAY "-- New Record? (Y/N) --".
+           ACCEPT FLG-LOOPING.
+
+
        204-IO-CLEANUP.
            CLOSE EMPLOYEE-FILE.
-
 
        END PROGRAM ASSIGNMENT-1.
